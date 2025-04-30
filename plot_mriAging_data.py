866944dddd31d79f0d564d1e1299d06b8c54b452
rdmagruder <rd.magruder@utah.edu>
2025-04-30 11:10:51 -0600

    Plotting Results

--- plot_mriAging_data.py
+++ plot_mriAging_data.py
@@ -93,14 +93,29 @@ for i, subject in enumerate(subjects):
         weighted_fat_fraction[i, j] = np.sum([data_dict[muscle]['Fat Fraction'][i, j] * data_dict[muscle]['Volume'][i, j] / total_volume for muscle in muscles])
 
 # Second timepoint
-timepoint_to_plot = 1 ; 
+timepoint_to_plot = 0
+velocity_to_plot = 0 #0, 1, 2, 3, 4 corresponds to 0, -60, -45, 90, 120
+remove_nans = True
+
 radial_diffusivity = weighted_diffusivity[:,timepoint_to_plot]
 fat_fraction = weighted_fat_fraction[:,timepoint_to_plot]
 volume_total = data_dict['Volume total'][:,timepoint_to_plot]
 contractile_volume = data_dict['contractile_volume'][:,timepoint_to_plot]
 peak_kem_stand = data_dict['Peak KEM Stand'][:,timepoint_to_plot]
+peak_kem_sit = data_dict['Peak KEM Sit'][:,timepoint_to_plot]
+peak_hfm_stand = data_dict['Peak HFM Stand'][:,timepoint_to_plot]
+peak_hfm_sit = data_dict['Peak HFM Sit'][:,timepoint_to_plot]
+peak_force_stand = data_dict['Peak Force Stand'][:,timepoint_to_plot]
+peak_force_sit = data_dict['Peak Force Sit'][:,timepoint_to_plot]
+peak_torso_ang_vel = data_dict['Peak Torso Ang Velocity'][:,timepoint_to_plot]
+torso_orientation_liftoff = data_dict['Torso Orientation at Liftoff'][:,timepoint_to_plot]
 age = data_dict['Age'][:,timepoint_to_plot]
 STS_time = data_dict['STS_time'][:,timepoint_to_plot]
+torque0 = data_dict[muscles[0]]['Torque'][:,timepoint_to_plot,0]
+torque1 = data_dict[muscles[0]]['Torque'][:,timepoint_to_plot,1]
+torque2 = data_dict[muscles[0]]['Torque'][:,timepoint_to_plot,2]
+torque3 = data_dict[muscles[0]]['Torque'][:,timepoint_to_plot,3]
+torque4 = data_dict[muscles[0]]['Torque'][:,timepoint_to_plot,4]
 
 # see if theres a nan in the row for either radial_diffusivity or peak_kem_stand. Remove that row from both vectors if theres a nan in that row of either vector
 no_nans = ~(np.isnan(peak_kem_stand) | np.isnan(radial_diffusivity))
@@ -111,6 +126,20 @@ contractile_volume = contractile_volume[no_nans]
 fat_fraction = fat_fraction[no_nans]
 age = age[no_nans]
 STS_time = STS_time[no_nans]
+peak_kem_sit = peak_kem_sit[no_nans]
+peak_hfm_stand = peak_hfm_stand[no_nans]
+peak_hfm_sit = peak_hfm_sit[no_nans]
+peak_force_stand = peak_force_stand[no_nans]
+peak_force_sit = peak_force_sit[no_nans]
+peak_torso_ang_vel = peak_torso_ang_vel[no_nans]
+torso_orientation_liftoff = torso_orientation_liftoff[no_nans]
+torque0 = torque0[no_nans]
+torque1 = torque1[no_nans]
+torque2 = torque2[no_nans]
+torque3 = torque3[no_nans]
+torque4 = torque4[no_nans]
+torques = np.column_stack((torque0, torque1, torque2, torque3, torque4))
+
 
 # multiple regression model to predict peak_kem_stand from some number of inputs
 import statsmodels.api as sm
@@ -118,7 +147,9 @@ import statsmodels.api as sm
 radial_diffusivity_standardized = (radial_diffusivity - np.mean(radial_diffusivity)) / np.std(radial_diffusivity)
 volume_total_standardized = (volume_total - np.mean(volume_total)) / np.std(volume_total)
 contractile_volume_standardized = (contractile_volume - np.mean(contractile_volume)) / np.std(contractile_volume)
-X = np.column_stack((radial_diffusivity_standardized, volume_total_standardized))
+
+# X = np.column_stack((torque0, torso_orientation_liftoff, peak_torso_ang_vel)) # only torque is sig
+X = np.column_stack((torque0, torque1, torque2, torque3, torque4))
 X = sm.add_constant(X)
 # fit the model
 model = sm.OLS(peak_kem_stand, X)
@@ -128,103 +159,101 @@ print(results.summary())
 
 
 # compute correlation between peak_kem_stand and radial_diffusivity
-x = radial_diffusivity_standardized + volume_total_standardized 
-# x = radial_diffusivity_standardized + contractile_volume_standardized 
-
-# x = results.fittedvalues
-# standardize x to have mean 0 and std 1
-# x = (x - np.mean(x)) / np.std(x)
-# x = results.fittedvalues
-# x = STS_time
-correlation = np.corrcoef(peak_kem_stand, x)[0, 1]
-
-# I want to statistically compare the strength of relationship between x and peak_kem_stand and x and STS_time
-# I will use a Steiger’s z test to compare the correlation coefficients
-from scipy.stats import norm
-def steiger_z_test(r1, r2, n):
-    # Calculate the z-score
-    z = (r1 - r2) / np.sqrt((1 - r1**2) / (n - 2) + (1 - r2**2) / (n - 2))
-    # Calculate the p-value
-    p_value = 2 * (1 - norm.cdf(np.abs(z)))
-    return z, p_value
-
-
-# Make a subplot of 3 plots. All with KEM on the x axis. The y axes will be 
-# 1. radial diffusivity
-# 2. volume total
-# 3. radial diffusivity * volume total
+# x = radial_diffusivity_standardized + volume_total_standardized
+# x = radial_diffusivity_standardized + contractile_volume_standardized
 x = radial_diffusivity_standardized + volume_total_standardized
 
-fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+# Define a function to make the 2 sets of subplots
+def plot_correls(y, ylabel, save_name1, save_name2):
+    # Make a subplot of 4 plots. All with input y on the y axis. the x axis will be kem_stand, kem_sit, hfm_stand, hfm_sit
+    fig, ax = plt.subplots(1, 4, figsize=(20, 5))
+    # Plot 1: y vs Peak KEM Stand
+    ax[0].scatter(peak_kem_stand, y)
+    ax[0].set_xlabel('Peak KEM Stand')
+    ax[0].set_ylabel(ylabel)
+    correlation1, p_value1 = pearsonr(peak_kem_stand, y)
+    ax[0].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation1, p_value1), transform=ax[0].transAxes, ha='center')
+    # Plot 2: y vs Peak KEM Sit
+    ax[1].scatter(peak_kem_sit, y)
+    ax[1].set_xlabel('Peak KEM Sit')
+    ax[1].set_ylabel(ylabel)
+    correlation2, p_value2 = pearsonr(peak_kem_sit, y)
+    ax[1].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation2, p_value2), transform=ax[1].transAxes, ha='center')
+    # Plot 3: y vs Peak HFM Stand
+    ax[2].scatter(peak_hfm_stand, y)
+    ax[2].set_xlabel('Peak HFM Stand')
+    ax[2].set_ylabel(ylabel)
+    correlation3, p_value3 = pearsonr(peak_hfm_stand, y)
+    ax[2].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation3, p_value3), transform=ax[2].transAxes, ha='center')
+    # Plot 4: y vs Peak HFM Sit
+    ax[3].scatter(peak_hfm_sit, y)
+    ax[3].set_xlabel('Peak HFM Sit')
+    ax[3].set_ylabel(ylabel)
+    correlation4, p_value4 = pearsonr(peak_hfm_sit, y)
+    ax[3].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation4, p_value4), transform=ax[3].transAxes, ha='center')
+    # Save the figure as an svg file with the name save_name1
+    plt.savefig(os.path.join(repo_dir, save_name1), format='svg', dpi=300)
+    plt.show()
 
-# Plot 1: Radial Diffusivity vs Peak KEM Stand
-ax[0].scatter(peak_kem_stand, radial_diffusivity)
-ax[0].set_xlabel('Peak KEM Stand')
-ax[0].set_ylabel('Radial Diffusivity')
-correlation1_kem, p_value1 = pearsonr(peak_kem_stand, radial_diffusivity)
-ax[0].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation1_kem, p_value1), transform=ax[0].transAxes, ha='center')
+    # Make a subplot of 3 plots. All with input y on the y axis. the x axis will be STS time, torso orientation at liftoff, peak torso ang vel
+    fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+    # Plot 1: y vs STS Time
+    ax[0].scatter(STS_time, y)
+    ax[0].set_xlabel('STS Time')
+    ax[0].set_ylabel(ylabel)
+    correlation5, p_value5 = pearsonr(STS_time, y)
+    ax[0].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation5, p_value5), transform=ax[0].transAxes, ha='center')
+    # Plot 2: y vs Torso Orientation at Liftoff
+    ax[1].scatter(torso_orientation_liftoff, y)
+    ax[1].set_xlabel('Torso Orientation at Liftoff')
+    ax[1].set_ylabel(ylabel)
+    correlation6, p_value6 = pearsonr(torso_orientation_liftoff, y)
+    ax[1].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation6, p_value6), transform=ax[1].transAxes, ha='center')
+    # Plot 3: y vs Peak Torso Ang Velocity
+    ax[2].scatter(peak_torso_ang_vel, y)
+    ax[2].set_xlabel('Peak Torso Ang Velocity')
+    ax[2].set_ylabel(ylabel)
+    correlation7, p_value7 = pearsonr(peak_torso_ang_vel, y)
+    ax[2].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation7, p_value7), transform=ax[2].transAxes, ha='center')
+    # Save the figure as an svg file with the name save_name2
+    plt.savefig(os.path.join(repo_dir, save_name2), format='svg', dpi=300)
+    plt.show()
 
-# Plot 2: Volume Total vs Peak KEM Stand
-ax[1].scatter(peak_kem_stand, volume_total)
-ax[1].set_xlabel('Peak KEM Stand')
-ax[1].set_ylabel('Volume Total')
-correlation2_kem, p_value2 = pearsonr(peak_kem_stand, volume_total)
-ax[1].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation2_kem, p_value2), transform=ax[1].transAxes, ha='center')
+# Plot correls for radial diffusivity standardized
+plot_correls(radial_diffusivity_standardized, 'Radial Diffusivity Standardized', 'RD_vs_moments_timepoint{}.svg'.format(timepoint_to_plot), 'RD_vs_STSmetrics_timepoint{}.svg'.format(timepoint_to_plot))
 
-# Plot 3: Radial Diffusivity * Volume Total vs Peak KEM Stand
-ax[2].scatter(peak_kem_stand, x)
-ax[2].set_xlabel('Peak KEM Stand')
-ax[2].set_ylabel('x (whatever you entered)')
-correlation3_kem, p_value3 = pearsonr(peak_kem_stand, x)
-ax[2].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation3_kem, p_value3), transform=ax[2].transAxes, ha='center')
+# Plot correls for volume total standardized
+plot_correls(volume_total_standardized, 'Volume Total Standardized', 'Vol_Total_vs_moments_timepoint{}.svg'.format(timepoint_to_plot), 'Vol_Total_vs_STSmetrics_timepoint{}.svg'.format(timepoint_to_plot))
 
+# Plot correls for contractile volume standardized
+plot_correls(contractile_volume_standardized, 'Contractile Volume Standardized', 'CV_vs_moments_timepoint{}.svg'.format(timepoint_to_plot), 'CV_vs_STSmetrics_timepoint{}.svg'.format(timepoint_to_plot))
+
+# Plot correls for fat fraction
+plot_correls(fat_fraction, 'Fat Fraction', 'FF_vs_moments_timepoint{}.svg'.format(timepoint_to_plot), 'FF_vs_STSmetrics_timepoint{}.svg'.format(timepoint_to_plot))
+
+# Plot correls for Radial Diffusivity * Volume Total
+plot_correls(x, 'Radial Diffusivity * Volume Total', 'RDxVol_vs_moments_timepoint{}.svg'.format(timepoint_to_plot), 'RDxVolvs_STSmetrics_timepoint{}.svg'.format(timepoint_to_plot))
+
+# Plot correls for all torques
+for i, torque in enumerate(torques.T):
+    plot_correls(torque, 'Torque {} deg/s'.format(velocities[i]), 'torque_{}deg_vs_moments_timepoint{}.svg'.format(velocities[i], timepoint_to_plot), 'torque_{}deg_vs_STSmetrics_timepoint{}.svg'.format(velocities[i], timepoint_to_plot))
+
+
+
+# Make a 1x5 subplot with torque velocities on the y and peak_kem_stand on the x
+fig, ax = plt.subplots(1, 5, figsize=(25, 5))
+# Plot 1: Torque vs Peak KEM Stand
+for i in range(5):
+    ax[i].scatter(peak_kem_stand, data_dict[muscles[0]]['Torque'][:,timepoint_to_plot,i][no_nans])
+    ax[i].set_xlabel('Peak KEM Stand')
+    # Use velocities to label the y axis
+    ax[i].set_ylabel('Torque at {} deg/s'.format(velocities[i]))
+    correlation1, p_value1 = pearsonr(peak_kem_stand, data_dict[muscles[0]]['Torque'][:,timepoint_to_plot,i][no_nans])
+    ax[i].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation1, p_value1), transform=ax[i].transAxes, ha='center')
 plt.show()
+# Save the figure as an svg file with the nam 'torque_velocities_vs_kem_stand.svg'
+plt.savefig(os.path.join(repo_dir, 'torque_velocities_vs_kem_stand_timepoint{}.svg'.format(timepoint_to_plot)), format='svg', dpi=300)
 
-# Make a subplot of 3 plots. All with STS Time on the x axis. The y axes will be 
-# 1. radial diffusivity
-# 2. volume total
-# 3. radial diffusivity * volume total
-
-fig, ax = plt.subplots(1, 3, figsize=(15, 5))
-
-# Plot 1: Radial Diffusivity vs STS Time
-ax[0].scatter(STS_time, radial_diffusivity)
-ax[0].set_xlabel('STS Time')
-ax[0].set_ylabel('Radial Diffusivity')
-correlation1_sts, p_value1 = pearsonr(STS_time, radial_diffusivity)
-ax[0].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation1_sts, p_value1), transform=ax[0].transAxes, ha='center')
-
-# Plot 2: Volume Total vs STS Time
-ax[1].scatter(STS_time, volume_total)
-ax[1].set_xlabel('STS Time')
-ax[1].set_ylabel('Volume Total')
-correlation2_sts, p_value2 = pearsonr(STS_time, volume_total)
-ax[1].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation2_sts, p_value2), transform=ax[1].transAxes, ha='center')
-
-# Plot 3: Radial Diffusivity * Volume Total vs STS Time
-ax[2].scatter(STS_time, x)
-ax[2].set_xlabel('STS Time')
-ax[2].set_ylabel('Radial Diffusivity * Volume Total')
-correlation3_sts, p_value3 = pearsonr(STS_time, x)
-ax[2].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation3_sts, p_value3), transform=ax[2].transAxes, ha='center')
-
-plt.show()
-
-# Make a 1x2 subplot with 1. kem vs x and 2. STS time vs x
-fig, ax = plt.subplots(1, 2, figsize=(10, 5))
-# Plot 1: Peak KEM Stand vs x
-ax[0].scatter(peak_kem_stand, x)
-ax[0].set_xlabel('Peak KEM Stand')
-ax[0].set_ylabel('x (whatever you entered)')
-correlation1, p_value1 = pearsonr(peak_kem_stand, x)
-ax[0].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation1, p_value1), transform=ax[0].transAxes, ha='center')
-# Plot 2: STS Time vs x
-ax[1].scatter(STS_time, x)
-ax[1].set_xlabel('STS Time')
-ax[1].set_ylabel('x (whatever you entered)')
-correlation2, p_value2 = pearsonr(STS_time, x)
-ax[1].text(0.5, 0.9, 'r= {:.2f}, p= {:.2e}'.format(correlation2, p_value2), transform=ax[1].transAxes, ha='center')
-plt.show()
 
 # I want to statistically compare the strength of relationship between x and peak_kem_stand and x and STS_time
 # I will use a Steiger’s z test to compare the correlation coefficients
@@ -236,43 +265,43 @@ def steiger_z_test(r1, r2, n):
     p_value = 2 * (1 - norm.cdf(np.abs(z)))
     return z, p_value
 
-# Calculate the z-score and p-value
-z, p_value = steiger_z_test(correlation3_kem, correlation3_sts, len(x))
-# Print the results
-print(f"Steiger's z-test composite score: z = {z:.2f}, p-value = {p_value:.2e}")
+# # Calculate the z-score and p-value
+# z, p_value = steiger_z_test(correlation3_kem, correlation3_sts, len(x))
+# # Print the results
+# print(f"Steiger's z-test composite score: z = {z:.2f}, p-value = {p_value:.2e}")
+#
+# # Calculate the steiger's test for radial diffusivity and volume total
+# z, p_value = steiger_z_test(correlation1_kem, correlation1_sts, len(x))
+# # Print the results
+# #print(f"Steiger's z-test radial diffusivity: z = {z:.2f}, p-value = {p_value:.2e}")
+# # Calculate the steiger's test for volume total
+# z, p_value = steiger_z_test(correlation2_kem, correlation2_sts, len(x))
+# # Print the results
+# #print(f"Steiger's z-test volume total: z = {z:.2f}, p-value = {p_value:.2e}")
 
-# Calculate the steiger's test for radial diffusivity and volume total 
-z, p_value = steiger_z_test(correlation1_kem, correlation1_sts, len(x))
-# Print the results
-#print(f"Steiger's z-test radial diffusivity: z = {z:.2f}, p-value = {p_value:.2e}")
-# Calculate the steiger's test for volume total
-z, p_value = steiger_z_test(correlation2_kem, correlation2_sts, len(x))
-# Print the results
-#print(f"Steiger's z-test volume total: z = {z:.2f}, p-value = {p_value:.2e}")
-
-# I want predicted x values for kem and STS time, then compute the errors, and compare them using
-# at t-test
-from sklearn.linear_model import LinearRegression
-# create a linear regression model
-model = LinearRegression()
-# fit the model to the data
-model.fit(peak_kem_stand.reshape(-1, 1), x)
-# get the predicted values
-predicted_kem = model.predict(peak_kem_stand.reshape(-1, 1))
-model2 = LinearRegression()
-# fit the model to the data
-model2.fit(STS_time.reshape(-1, 1), x)
-# get the predicted values
-predicted_sts = model2.predict(STS_time.reshape(-1, 1))
-# compute the errors
-error_kem = np.abs(x - predicted_kem)
-error_sts = np.abs(x - predicted_sts)
-print(error_kem)
-print(error_sts)
-# paired samples t-test
-from scipy.stats import ttest_rel
-# Perform the paired t-test
-t_stat, p_value = ttest_rel(error_kem, error_sts)
-# Print the results
-print(f"t-statistic: {t_stat:.2f}, p-value: {p_value:.4e}")
+# # I want predicted x values for kem and STS time, then compute the errors, and compare them using
+# # at t-test
+# from sklearn.linear_model import LinearRegression
+# # create a linear regression model
+# model = LinearRegression()
+# # fit the model to the data
+# model.fit(peak_kem_stand.reshape(-1, 1), x)
+# # get the predicted values
+# predicted_kem = model.predict(peak_kem_stand.reshape(-1, 1))
+# model2 = LinearRegression()
+# # fit the model to the data
+# model2.fit(STS_time.reshape(-1, 1), x)
+# # get the predicted values
+# predicted_sts = model2.predict(STS_time.reshape(-1, 1))
+# # compute the errors
+# error_kem = np.abs(x - predicted_kem)
+# error_sts = np.abs(x - predicted_sts)
+# print(error_kem)
+# print(error_sts)
+# # paired samples t-test
+# from scipy.stats import ttest_rel
+# # Perform the paired t-test
+# t_stat, p_value = ttest_rel(error_kem, error_sts)
+# # Print the results
+# print(f"t-statistic: {t_stat:.2f}, p-value: {p_value:.4e}")
 
